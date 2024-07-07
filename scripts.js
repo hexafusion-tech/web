b6672c9f6ecfe2c7621ff1546c8a355297ef5aea
hexafusion-tech <hexafusion.tech@gmail.com>
2024-07-07 18:34:22 +0530

    Delete scripts.js
    Signed-off-by: hexafusion-tech <hexafusion.tech@gmail.com>

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('contact-form');
-
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    alert('Form submitted successfully!');
-    form.reset();
-  });
-});
